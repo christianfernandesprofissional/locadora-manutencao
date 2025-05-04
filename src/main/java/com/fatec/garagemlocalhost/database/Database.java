c5bc5a9d2eb0a90a9d5683c562e118b147cc67a2
christianfernandesprofissional <christianfernandesprofissional@gmail.com>
2025-05-03 22:49:23 -0300

    feat: implementação da classe Database

--- src/main/java/com/fatec/garagemlocalhost/database/Database.java
+++ src/main/java/com/fatec/garagemlocalhost/database/Database.java
@@ -4,10 +4,32 @@
  */
 package com.fatec.garagemlocalhost.database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author chris
  */
 public class Database {
+    private static final String URL = System.getenv("DB_URL");
+    private static final String USER = System.getenv("DB_USER");
+    private static final String PASSWORD = System.getenv("DB_PASSWORD");
+    
+    private Connection conn = null; 
+    
+    public Database(){
+        try{
+            conn = DriverManager.getConnection(URL,USER,PASSWORD);
+            System.out.println("Conexão bem sucedida!");
+        }catch(SQLException e){
+            System.out.println("Falha na conexão: " + e.getMessage());
+        }
+    }
+
+    public Connection getConn() {
+        return conn;
+    }
     
 }
