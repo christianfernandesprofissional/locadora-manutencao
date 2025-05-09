00680e67195d8dd70ad653e72b0b5db99460c588
christianfernandesprofissional <christianfernandesprofissional@gmail.com>
2025-05-09 14:21:17 -0300

    fix: deleção do module-path

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.mycompany.locadoramanutencao {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-    opens com.fatec.garagemlocalhost to javafx.fxml;
-    exports com.fatec.garagemlocalhost;
-}
