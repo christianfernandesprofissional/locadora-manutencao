3df019d7b42b9bd5c67745db2d1ac85b337c25d9
christianfernandesprofissional <christianfernandesprofissional@gmail.com>
2025-05-09 21:41:36 -0300

    feat: LoginValidacaoException adicionado

---
+++ src/main/java/com/fatec/garagemlocalhost/exceptions/LoginValidacaoException.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.fatec.garagemlocalhost.exceptions;
+
+/**
+ *
+ * @author chris
+ */
+public class LoginValidacaoException extends Exception{
+    
+    public LoginValidacaoException(String msg){
+        super(msg);
+    }
+    
+}
