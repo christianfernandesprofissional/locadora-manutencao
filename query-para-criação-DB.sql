43c4812d638391a8182b845f3ec5d55f490b1401
christianfernandesprofissional <christianfernandesprofissional@gmail.com>
2025-05-27 09:07:57 -0300

    fix: alteração na query de criação do banco

--- query-para-criação-DB.sql
+++ query-para-criação-DB.sql
@@ -27,7 +27,8 @@ CREATE TABLE usuarios (
     nome VARCHAR(80) NOT NULL, 
     email VARCHAR(80) UNIQUE NOT NULL,
     senha VARCHAR(80) NOT NULL,
-    tipo_usuario INT NOT NULL
+    tipo_usuario INT NOT NULL,
+    ativo BOOLEAN DEFAULT TRUE
 );
 
 CREATE TABLE clientes (
@@ -55,9 +56,10 @@ CREATE TABLE veiculos (
     chassi VARCHAR(17) UNIQUE NOT NULL, 
     modelo VARCHAR(30) NOT NULL, 
     quilometragem INT NOT NULL, 
-    id_categoria INT NOT NULL, 
+    id_categoria INT, 
     preco_base DECIMAL(10,2) NOT NULL, 
     CONSTRAINT fk_veiculo_categoria FOREIGN KEY(id_categoria) REFERENCES categorias_veiculos(id_categoria)
+    ON DELETE CASCADE
     ON UPDATE CASCADE
 );
 
@@ -65,13 +67,14 @@ CREATE TABLE veiculos (
 
 CREATE TABLE manutencoes (
 	id_manutencao INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-	placa CHAR(7) NOT NULL, 
+	placa CHAR(7), 
 	descricao TEXT NOT NULL, 
 	finalizado BOOLEAN DEFAULT FALSE, 
 	instante_chegada DATETIME, 
 	instante_saida DATETIME,
 	total DECIMAL(10,2),
 	CONSTRAINT fk_manutencao_placa FOREIGN KEY(placa) REFERENCES veiculos(placa)
+    ON DELETE SET NULL
 	ON UPDATE CASCADE
 );
 
@@ -88,9 +91,9 @@ CREATE TABLE manutencoes_servicos(
     id_servico INT,
     valor_item DECIMAL(10,2),
     CONSTRAINT fk_ms_manutencao FOREIGN KEY(id_manutencao) REFERENCES manutencoes(id_manutencao)
-    ON DELETE CASCADE ON UPDATE CASCADE, 
+    ON DELETE SET NULL ON UPDATE CASCADE, 
     CONSTRAINT fk_ms_servico FOREIGN KEY(id_servico) REFERENCES servicos(id_servico)
-    ON DELETE CASCADE ON UPDATE CASCADE
+    ON DELETE SET NULL ON UPDATE CASCADE
 );
 
 -- TABELAS DE LOCAÇÃO
@@ -100,7 +103,7 @@ CREATE TABLE pedidos_locacao (
     id_atendente INT NOT NULL,
     id_cliente INT NOT NULL,
     id_seguro INT,
-    placa CHAR(7) NOT NULL, 
+    placa CHAR(7), 
     id_saida INT,
     id_devolucao INT, 
     devolucao_esperada DATE NOT NULL, 
@@ -112,10 +115,12 @@ CREATE TABLE pedidos_locacao (
 	CONSTRAINT fk_pedido_cliente FOREIGN KEY(id_cliente) REFERENCES clientes(id_cliente)
     ON UPDATE CASCADE, 
     CONSTRAINT fk_pedido_placa FOREIGN KEY(placa) REFERENCES veiculos(placa)
+    ON DELETE SET NULL
     ON UPDATE CASCADE, 
     CONSTRAINT fk_pedido_pagamento FOREIGN KEY(forma_de_pagamento) REFERENCES meios_de_pagamento(id_pagamento)
     ON UPDATE CASCADE,
     CONSTRAINT fk_pedido_seguro FOREIGN KEY(id_seguro) REFERENCES tipos_seguro(id_seguro)
+    ON DELETE SET NULL
     ON UPDATE CASCADE
 );
 
@@ -123,15 +128,17 @@ CREATE TABLE pedidos_locacao (
 
 CREATE TABLE devolucoes_veiculos (
 	id_devolucao INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-	id_pedido INT NOT NULL,
+	id_pedido INT,
 	id_assistente INT,
 	instante_devolucao DATETIME, 
-    placa CHAR(7) NOT NULL,
+    placa CHAR(7),
 	km_chegada INT,
 	id_manutencao INT, 
     CONSTRAINT fk_devolucao_placa FOREIGN KEY(placa) REFERENCES veiculos(placa)
-    ON DELETE CASCADE ON UPDATE CASCADE,
+	ON DELETE SET NULL 
+    ON UPDATE CASCADE,
 	CONSTRAINT fk_devolucao_pedido FOREIGN KEY(id_pedido) REFERENCES pedidos_locacao(id_pedido)
+    ON DELETE SET NULL
 	ON UPDATE CASCADE,
 	CONSTRAINT fk_devolucao_assistente FOREIGN KEY(id_assistente) REFERENCES usuarios(id_usuario)
 	ON UPDATE CASCADE, 
@@ -141,14 +148,16 @@ CREATE TABLE devolucoes_veiculos (
 
 CREATE TABLE saidas_veiculos (
 	id_saida INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-	id_pedido INT NOT NULL,
-	id_assistente INT NOT NULL, 
-    placa CHAR(7) NOT NULL,
+	id_pedido INT,
+	id_assistente INT, 
+    placa CHAR(7),
 	instante_saida DATETIME, 
 	km_saida INT,
     CONSTRAINT fk_saida_placa FOREIGN KEY(placa) REFERENCES veiculos(placa)
-    ON DELETE CASCADE ON UPDATE CASCADE,
+    ON DELETE SET NULL
+    ON UPDATE CASCADE,
 	CONSTRAINT fk_saida_pedido FOREIGN KEY(id_pedido) REFERENCES pedidos_locacao(id_pedido)
+	ON DELETE SET NULL
 	ON UPDATE CASCADE,
 	CONSTRAINT fk_saida_assistente FOREIGN KEY(id_assistente) REFERENCES usuarios(id_usuario)
 	ON UPDATE CASCADE
