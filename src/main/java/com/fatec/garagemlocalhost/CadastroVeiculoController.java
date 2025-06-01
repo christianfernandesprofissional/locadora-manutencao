4d5dcdd7fc8036f3e26fec64be292a21fa0bba72
christianfernandesprofissional <christianfernandesprofissional@gmail.com>
2025-06-01 12:08:19 -0300

    feat: adicionado tela de cadastro de veículos e CadastroVeiculoController, Closes #38

---
+++ src/main/java/com/fatec/garagemlocalhost/CadastroVeiculoController.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.fatec.garagemlocalhost;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+/**
+ *
+ * @author chris
+ */
+public class CadastroVeiculoController {
+    
+    @FXML
+    private Button btnLimpar;
+
+    @FXML
+    private Button btnSalvar;
+
+    @FXML
+    private ComboBox<?> cmbCategoria;
+
+    @FXML
+    private TextField txtAno;
+
+    @FXML
+    private TextField txtChassi;
+
+    @FXML
+    private TextField txtCor;
+
+    @FXML
+    private TextField txtMarca;
+
+    @FXML
+    private TextField txtModelo;
+
+    @FXML
+    private TextField txtPlaca;
+
+    @FXML
+    private TextField txtPreco;
+
+    @FXML
+    private TextField txtQuilometragem;
+
+}
