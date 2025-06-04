57ceaa5c868d4b048fcbaeee672fa98ef5505ec6
christianfernandesprofissional <christianfernandesprofissional@gmail.com>
2025-06-03 23:01:53 -0300

    feat: TelaInicialController adicionado

---
+++ src/main/java/com/fatec/garagemlocalhost/TelaInicialController.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.fatec.garagemlocalhost;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ *
+ * @author chris
+ */
+public class TelaInicialController implements Initializable{
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+       
+    }
+    
+}
