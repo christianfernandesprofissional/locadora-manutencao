eae407e664c60ddc594f0ce63f9bf21872b4cac2
christianfernandesprofissional <christianfernandesprofissional@gmail.com>
2025-06-03 23:03:14 -0300

    fix: atualizações no LoginController

--- src/main/java/com/fatec/garagemlocalhost/LoginController.java
+++ src/main/java/com/fatec/garagemlocalhost/LoginController.java
@@ -56,47 +56,59 @@ public class LoginController implements Initializable {
     private final UsuarioService usuarioService = new UsuarioService();
 
     @FXML
-    public void logar(ActionEvent event) throws IOException {
-        
+    public void logar(ActionEvent event) {
+
         lblLoginInvalido.setVisible(false);
+
         try {
             Optional<Usuario> usuarioBuscado = usuarioService.buscarPorNome(txtUsuario.getText());
-
-            if (usuarioBuscado.isPresent()) {
-                if (usuarioBuscado.get().getSenha().equals(txtUnmaskSenha.getText())) {
-                    // Fecha a janela de login
-                    Stage stageAtual = (Stage) ((Node) event.getSource()).getScene().getWindow();
-                    stageAtual.close();
-
-                    //Carrega a tela home
-                    FXMLLoader loader = new FXMLLoader(getClass().getResource("home.fxml"));
-                    Parent root = loader.load();
-                    Stage novoStage = new Stage();
-                    novoStage.setTitle("Home");
-                    novoStage.setScene(new Scene(root));
-                    novoStage.setFullScreen(true);
-                    novoStage.show();
-                    
-                } else {
-                    lblLoginInvalido.setText("Usuário ou senha inválidos!");
-                    lblLoginInvalido.setVisible(true);
-                    txtUsuario.setText("");
-                    txtUnmaskSenha.setText("");
-                    txtUsuario.requestFocus();
-                }
+            if (usuarioBuscado.isPresent() && !usuarioBuscado.get().getAtivo()) {
+                lblLoginInvalido.setText("Usuário bloqueado, \n fale com o gerente!");
+                lblLoginInvalido.setVisible(true);
+                txtUsuario.setText("");
+                txtUnmaskSenha.setText("");
+                txtUsuario.requestFocus();
             } else {
-                lblLoginInvalido.setText("Usuário não cadastrado!");
+                validarLogin(usuarioBuscado, event);
+            }
+
+        } catch (DBException | LoginValidacaoException | IOException e) {
+            Alert alert = new Alert(AlertType.ERROR, e.getMessage(), ButtonType.CLOSE);
+            alert.showAndWait();
+        }
+
+    }
+
+    private void validarLogin(Optional<Usuario> usuarioBuscado, ActionEvent event) throws IOException {
+        if (usuarioBuscado.isPresent()) {
+            if (usuarioBuscado.get().getSenha().equals(txtUnmaskSenha.getText())) {
+                // Fecha a janela de login
+                Stage stageAtual = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                stageAtual.close();
+
+                //Carrega a tela home
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("home.fxml"));
+                Parent root = loader.load();
+                Stage novoStage = new Stage();
+                novoStage.setTitle("Home");
+                novoStage.setScene(new Scene(root));
+                novoStage.setFullScreen(true);
+                novoStage.show();
+
+            } else {
+                lblLoginInvalido.setText("Usuário ou senha inválidos!");
                 lblLoginInvalido.setVisible(true);
                 txtUsuario.setText("");
                 txtUnmaskSenha.setText("");
                 txtUsuario.requestFocus();
             }
-
-        } catch (DBException | LoginValidacaoException e) {
-            Alert alert = new Alert(AlertType.ERROR, e.getMessage(),ButtonType.CLOSE);
-            alert.showAndWait();
+        } else {
+            lblLoginInvalido.setText("Usuário não cadastrado!");
+            lblLoginInvalido.setVisible(true);
+            txtUsuario.setText("");
+            txtUnmaskSenha.setText("");
+            txtUsuario.requestFocus();
         }
-
     }
 
     /**
