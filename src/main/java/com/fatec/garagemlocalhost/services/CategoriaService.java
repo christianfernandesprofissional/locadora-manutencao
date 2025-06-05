8283ed904486a870a48be564e1bf7a55e2bb1b55
guuhsantoss <guuhsantoss@hotmail.com>
2025-06-05 09:38:26 -0300

    Feat: Criada a classe CategoriaService, Closes #51

--- src/main/java/com/fatec/garagemlocalhost/services/CategoriaService.java
+++ src/main/java/com/fatec/garagemlocalhost/services/CategoriaService.java
@@ -5,10 +5,12 @@
 package com.fatec.garagemlocalhost.services;
 
 import com.fatec.garagemlocalhost.database.DBException;
+import com.fatec.garagemlocalhost.database.Database;
 import com.fatec.garagemlocalhost.exceptions.CampoVazioException;
 import com.fatec.garagemlocalhost.exceptions.IdInvalidoException;
 import com.fatec.garagemlocalhost.model.dao.CategoriaVeiculoDAO;
 import com.fatec.garagemlocalhost.model.entities.CategoriaVeiculo;
+import com.fatec.garagemlocalhost.utils.Verificar;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
@@ -21,6 +23,11 @@ public class CategoriaService {
 
     private CategoriaVeiculoDAO categoriaVeiculoDao;
 
+    public CategoriaService(){
+        Database db = new Database();
+        categoriaVeiculoDao = new CategoriaVeiculoDAO(db);
+    }
+
     public List<CategoriaVeiculo> buscarTodasCategorias() throws DBException {
         try {
             return categoriaVeiculoDao.findAll();
@@ -28,6 +35,7 @@ public class CategoriaService {
             throw new DBException("Falha ao Encontrar Categorias");
         }
     }
+
     public Optional<CategoriaVeiculo> buscarPorId(Integer id) throws DBException, IdInvalidoException {
         try {
             if (id <= 0) {
@@ -38,30 +46,44 @@ public class CategoriaService {
             throw new DBException("Erro ao encontrar ID da Categoria");
         }
     }
+
     public void criarCategoriaVeiculo(CategoriaVeiculo c) throws DBException {
-        try{        
-            //CRIAR A LOGICA DOS CAMPOS, USAR O METODO QUE O CHRISTIAN ESTA FAZENDO
-            throw new DBException("Erro a Categoria nao foi criada");
-            categoriaVeiculoDao.createCategoria(c);
-        }catch(SQLException e) {
+        try {
+            if(!Verificar.todosAtributosPreenchidos(c, "getId") ){
+                throw new DBException("Erro a Categoria nao foi criada, pois existem campos vazios");
+            }
+                categoriaVeiculoDao.createCategoria(c);     
+        } catch (SQLException e) {
             throw new DBException("Erro ao criar Categoria");
         }
     }
-    public void atualiarCategoriaVeiculo(CategoriaVeiculo c) throws DBException{
-        try{
-            throw new CampoVazioException("Preencha todos os campos para prosseguir");
-        }catch(SQLException e){
+
+    public void atualizarCategoriaVeiculo(CategoriaVeiculo c) throws DBException {
+        try {
+            //Verificar Retorna TRUE caso todos os valores estejam preenchidos
+            if(!Verificar.todosAtributosPreenchidos(c)){
+                throw new CampoVazioException("Erro a Categoria nao foi atualizada, pois existem campos vazios");
+            }
+                categoriaVeiculoDao.updateCategoria(c); 
+        } catch (SQLException e) {
             throw new DBException("Erro ao atualizar CategoriaVeiculo");
         }
     }
-    public void deletarCategoriaVeiculoPorId(CategoriaVeiculo  c) throws DBException{
-        try{
-            
-        }catch(SQLException e ){
+
+    public void deletarCategoriaVeiculoPorId(CategoriaVeiculo c, Integer id) throws DBException {
+        try {
+            if(id < 0){
+                throw new DBException("Erro o ID nao pode ser menor que 0 (zero)");
+            }
+            if(!categoriaVeiculoDao.findById(id).isPresent()){
+               throw new DBException("Categoria nao encontrada");
+            }
+                categoriaVeiculoDao.deleteCategoria(id);      
+        } catch (SQLException e) {
             throw new DBException("Erro ao deletar CategoriaVeiculo");
         }
     }
-    public void deletarCategoriaVeiculoTudo(CategoriaVeiculo c) throws DBException{
-        
+
+    public void deletarCategoriaVeiculoTudo(CategoriaVeiculo c) throws DBException {
     }
 }
