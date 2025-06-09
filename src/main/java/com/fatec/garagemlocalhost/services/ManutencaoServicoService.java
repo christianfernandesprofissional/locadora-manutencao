baef452a6c8a00a0427822e8a31ef441c1eadff0
guuhsantoss <guuhsantoss@hotmail.com>
2025-06-09 09:28:03 -0300

    Feat: Criada a classe ManutencaoServicoService, Closes#61

--- src/main/java/com/fatec/garagemlocalhost/services/ManutencaoServicoService.java
+++ src/main/java/com/fatec/garagemlocalhost/services/ManutencaoServicoService.java
@@ -4,10 +4,41 @@
  */
 package com.fatec.garagemlocalhost.services;
 
+import com.fatec.garagemlocalhost.database.DBException;
+import com.fatec.garagemlocalhost.database.Database;
+import com.fatec.garagemlocalhost.exceptions.CampoVazioException;
+import com.fatec.garagemlocalhost.model.dao.ManutencaoServicoDAO;
+import com.fatec.garagemlocalhost.model.entities.Manutencao;
+import com.fatec.garagemlocalhost.utils.Verificar;
+import java.sql.SQLException;
+
+
 /**
  *
  * @author gustavo
  */
 public class ManutencaoServicoService {
+    private ManutencaoServicoDAO manutencaoServicoDao;
     
+    public ManutencaoServicoService(){
+    Database db = new Database();
+    manutencaoServicoDao = new ManutencaoServicoDAO(db);
+}
+    public void preencherManutencaoServico(Manutencao manutencao) throws DBException, CampoVazioException {
+        try{
+            if(!Verificar.todosAtributosPreenchidos(manutencao)){
+                throw new CampoVazioException("Existem Campos vazios no objeto MANUTENCAO, ou ela nao existe");
+            }
+            manutencaoServicoDao.preencherServicos(manutencao);
+        }catch(SQLException e){
+            throw new DBException("Erro ao preencher ManutencaoServico");
+        }      
+    }
+    public void insereManutencaoServico(Manutencao manutencao) throws DBException{
+        try{
+            manutencaoServicoDao.insereServicos(manutencao);
+        }catch(SQLException e){
+            throw new DBException("Erro ao Inserir Manutencao");
+        }
+    }
 }
